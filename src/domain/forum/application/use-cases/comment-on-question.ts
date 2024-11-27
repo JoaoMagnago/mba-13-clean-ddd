6587ff638685d0546c38bb8fbd4cde2455f5c2de
joao magnago <joaomagnago.wm@gmail.com>
2024-11-26 23:22:54 -0300

    feat(comment): create comment on answer use case

--- src/domain/forum/application/use-cases/comment-on-question.ts
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
-import { QuestionsCommentsRepository } from '../repositories/question-comments-repository'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
 import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface CommentOnQuestionUseCaseRequest {
@@ -16,7 +16,7 @@ interface CommentOnQuestionUseCaseResponse {
 export class CommentOnQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
-    private questionCommentsRepository: QuestionsCommentsRepository,
+    private questionCommentsRepository: QuestionCommentsRepository,
     // eslint-disable-next-line prettier/prettier
   ) { }
 
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,8 +1,8 @@
-import { QuestionsCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
 // eslint-disable-next-line prettier/prettier
-export class InMemoryQuestionCommentsRepository implements QuestionsCommentsRepository {
+export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
   public items: QuestionComment[] = []
 
   async create(questionComment: QuestionComment) {
